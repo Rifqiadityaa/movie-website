837463d2ec3578d82877d2f3aeedc9e4d9620a9b
Rifqi Aditya <rifqiadityaa24@gmail.com>
2025-07-17 16:52:49 +0700

    feat: create new constants for cache key and tmdb img

---
+++ src/constants/cacheKey.ts
@@ -0,0 +1,10 @@
+const REACT_QUERY_CACHE_KEYS = {
+  getPopularMovies: "getPopularMovies",
+  getMoviesGenres: "getMoviesGenres",
+  getMovieDetail: "getMovieDetail",
+  getMoviesByFilter: "getMoviesByFilter",
+  getMovieReviews: "getMovieReviews",
+  getMovieCast: "getMovieCast",
+};
+
+export default REACT_QUERY_CACHE_KEYS;
---
+++ src/constants/tmdb.ts
@@ -0,0 +1,17 @@
+export const TMDB_IMG_BASE_URL = "https://image.tmdb.org/t/p";
+
+export const TMDB_IMG_RES = {
+  backdrop: {
+    w1280: "/w1280",
+    original: "/original",
+  },
+  poster: {
+    w500: "/w500",
+    w780: "/w780",
+    original: "/original",
+  },
+  profile: {
+    w185: "/w185",
+    h632: "/h632",
+  },
+};
